420e68b744cc11cbf6d073893e740de9b4827bbe
suneyra ozgur <sozgur@Sultans-MacBook-Pro-2.local>
2014-08-21 13:49:59 +0300

    chapter 5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+
+  get 'home', to: 'static_pages#home', as:'home'
+  get 'help', to: 'static_pages#help', as:'help'
+  get 'about', to: 'static_pages#about', as:'about'
+  get 'contact', to: 'static_pages#contact', as:'contact'
+
+  get 'new', to: 'users#new', as:'new'
  
 
   # The priority is based upon order of creation: first created -> highest priority.
