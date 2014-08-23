7fee412dd1968bf6629e13896b4fe0fa62f55030
sumeyra ozgur <sozgur@Sultans-MacBook-Pro-2.local>
2014-08-23 14:05:41 +0300

    Finish user edit, update, index, and destroy actions

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as:'signup'
   get 'signin', to: 'sessions#new', as:'signin'
   delete 'signout', to: 'sessions#destroy'
- 
 
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
