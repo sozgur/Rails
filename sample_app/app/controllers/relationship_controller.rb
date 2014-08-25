9fd8e8c0ef25f18595077680e4cccee23de5ac62
sumeyra ozgur <sozgur@Sultans-MacBook-Pro-2.local>
2014-08-25 17:07:57 +0300

    follow buton olmadi

--- sample_app/app/controllers/relationship_controller.rb
+++ sample_app/app/controllers/relationship_controller.rb
@@ -1,19 +1,17 @@
 class RelationshipsController < ApplicationController
   before_action :signed_in_user
 
-  def create
+  respond_to :html, :js
+
+ def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-    format.html {redirect_to @user}
-    format.js
+    respond_with @user
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    respond_with @user
   end
 end
