148b97709e19ddf3282d56e3e73576d4257a3e41
sozgur <smyrozgur@gmail.com>
2016-01-15 00:20:14 +0200

    Rails

--- demo_app/app/models/micropost.rb
+++ demo_app/app/models/micropost.rb
@@ -1,4 +1,26 @@
 class Micropost < ActiveRecord::Base
 	belongs_to :user
 	validates :content, length: { maximum: 20 }
+	has_many :taggings
+    has_many :tags, through: :taggings
+
+    def self.tagged_with(name)
+      Tag.find_by_name!(name).microposts
+    end
+
+    def self.tag_counts
+      Tag.select("tags.*, count(taggings.tag_id) as count").
+      joins(:taggings).group("taggings.tag_id")
+    end
+  
+    def tag_list
+      tags.map(&:name).join(", ")
+    end
+  
+    def tag_list=(names)
+      self.tags = names.split(",").map do |n|
+      Tag.where(name: n.strip).first_or_create!
+    end
+  end
+
 end
